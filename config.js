0556d47a5c8a0bd78d130c5138bdac606175f6ee
adcpho12 <94826169+adcpho12@users.noreply.github.com>
2021-11-22 16:48:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
-    titleWeb: "LưuHuyềnTrang",
+    titleWeb: "HuyềnTrang",
     introTitle: 'Híllu Trang',
     introDesc: `Có câu này muốn hỏi Trang
     Nhưng phải trở lời thật lòng nha`,
     btnIntro: '^^Okiii^^',
     title: 'Trang có yêu mình hôngggg 🥰',
     desc: 'Thoát ra là yêu mình lắm đó nha ',
-    btnYes: 'có. Yêu lắm <33',
+    btnYes: 'Có. Yêu lắm <33',
     btnNo: 'Không nha, mơ à :3',
     question: 'Thôi đùa đấy. Giờ trả lời thật đi có yêu mình không <3',
     btnReply: 'Trả lời câu hỏi',
@@ -14,5 +14,5 @@ const CONFIG = {
     mess: 'Hí hí biết mà 🥰. iu iu 😘😘',
     messDesc: 'Thế để chế độ hẹn hò luôn nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/tetjluta6725' 
+    messLink: 'https://www.facebook.com/trangtit2k4' 
 }
